c2694c50df36a01f3a2f892a0d80835e07be4f56
samishv <svelazquezo1801@alumno.ipn.mx>
2024-09-30 20:59:40 -0600

    añadimos archivo

---
+++ Clase1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 10:06:21 2024
+
+@author: samish
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import data, io
+
+# Aquí comentario
+
+imagen = io.imread(r'C:\Users\ikerf\Desktop\Upiita\7mo semestre\Patrones\Imagenes\objetos1.jpg')
+# imagen = data.astronaut()
+Parte=imagen[0:50,0:50,:]
+plt.figure(0)
+plt.imshow(imagen)
+plt.figure(1)
+plt.imshow(Parte)
+
+hr = np.zeros(256) #histograma rojos
+hv = np.zeros(256) #istograma verdes
+ha = np.zeros(256)
+
+filas = imagen.shape[0]
+columnas = imagen.shape[1]
+capas = imagen.shape[2]
+
+# for i in range(0,filas,1):
+#     for j in range(0,columnas,2):
+#         posR = imagen
